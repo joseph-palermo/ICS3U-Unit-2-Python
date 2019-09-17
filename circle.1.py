5d38799b1c5ce783a949e0cd800b1b7d8265cbe5
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-09-17 18:35:20 +0000

    Inital commit

---
+++ circle.1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Joseph Palermo
+# Created on: September 2019
+# My circle pi program
+
+
+import math
+
+
+def main():
+    print("math.pi*15**2={}" .format(math.pi*15**2))
+    print("2*math.pi*15={}" .format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+    main()
